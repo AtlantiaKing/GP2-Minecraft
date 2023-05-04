89dd5fa32ca64dbe92981d7d7bb39eb8353968d7
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-05-04 12:57:46 +0200

    Refactored the BlockInteractionComponent
    Fixed items with hardness 0 spawning the block breaking renderer
    Added comments
    Refactored the Update method of the BlockInteractionComponent

--- Project/OverlordProject/Components/BlockInteractionComponent.cpp
+++ Project/OverlordProject/Components/BlockInteractionComponent.cpp
@@ -20,6 +20,7 @@ void BlockInteractionComponent::Update(const SceneContext& sceneContext)
 	TransformComponent* pCamera{ sceneContext.pCamera->GetTransform() };
 	constexpr float playerBlockRadius{ 5.0f };
 
+	// Get camera/ray information
 	const XMFLOAT3 cameraPosition{ pCamera->GetWorldPosition() };
 	const PxVec3 raycastOrigin{ cameraPosition.x, cameraPosition.y, cameraPosition.z };
 	const XMFLOAT3 cameraForward{ pCamera->GetForward() };
@@ -29,79 +30,101 @@ void BlockInteractionComponent::Update(const SceneContext& sceneContext)
 	filter.data.word0 = static_cast<PxU32>(CollisionGroup::World);
 
 	PxRaycastBuffer hit;
-	if (m_PxScene->raycast(raycastOrigin, raycastDirection, playerBlockRadius, hit, PxHitFlag::eDEFAULT, filter))
+	if (!m_PxScene->raycast(raycastOrigin, raycastDirection, playerBlockRadius, hit, PxHitFlag::eDEFAULT, filter))
 	{
-		if (!hit.hasBlock)
+		// If we are not looking at a block, disable both the break renderer and the selection renderer
+		m_pSelection->SetVisibility(false);
+		m_pBreakRenderer->SetVisibility(false);
+
+		// Reset block breaking
+		m_IsBreakingBlock = false;
+
+		return;
+	}
+
+	// If the raycast has no information
+	if (!hit.hasBlock)
+	{
+		// Disable both the break renderer and the selection renderer
+		m_pSelection->SetVisibility(false);
+		m_pBreakRenderer->SetVisibility(false);
+		return;
+	}
+
+	// Enable the selection renderer
+	m_pSelection->SetVisibility(true);
+
+	// Get the position of the block that we are looking at
+	const PxVec3 hitPos{ hit.block.position + raycastDirection * 0.01f };
+	const XMFLOAT3 blockPos
+	{
+		(hitPos.x + 0.5f) > 0.0f ? floor(hitPos.x + 0.5f) : -(floor(abs(hitPos.x + 0.5f)) + 1),
+		(hitPos.y + 0.5f) > 0.0f ? floor(hitPos.y + 0.5f) : -(floor(abs(hitPos.y + 0.5f)) + 1),
+		(hitPos.z + 0.5f) > 0.0f ? floor(hitPos.z + 0.5f) : -(floor(abs(hitPos.z + 0.5f)) + 1)
+	};
+	// Move the selection renderer to the current block position
+	m_pSelection->GetTransform()->Translate(blockPos);
+
+	// Get current block
+	Block* pBlock{ m_pWorld->GetBlockAt(static_cast<int>(blockPos.x), static_cast<int>(blockPos.y), static_cast<int>(blockPos.z)) };
+
+	// If we changed the block we were looking at, reset block breaking
+	if (HasChangedPosition(blockPos)) m_IsBreakingBlock = false;
+
+	// RIGHT MOUSE CLICK
+	if (InputManager::IsMouseButton(InputState::pressed, 2))
+	{
+		// Place a block
+		m_pWorld->PlaceBlock(XMFLOAT3{ hitPos.x, hitPos.y, hitPos.z }, blockPos, BlockType::DIRT);
+
+		// Reset block breaking
+		m_IsBreakingBlock = false;
+	}
+
+	// LEFT MOUSE CLICK
+	if (InputManager::IsMouseButton(InputState::down, 1))
+	{
+		// If we are breaking a block
+		if (m_IsBreakingBlock && pBlock)
 		{
-			m_pSelection->SetVisibility(false);
-			return;
-		}
+			// Increment breaking time
+			m_BlockBreakProgress += sceneContext.pGameTime->GetElapsed();
 
-		m_pSelection->SetVisibility(true);
-
-		const PxVec3 hitPos{ hit.block.position + raycastDirection * 0.01f };
-		const XMFLOAT3 blockPos
-		{
-			(hitPos.x + 0.5f) > 0.0f ? floor(hitPos.x + 0.5f) : -(floor(abs(hitPos.x + 0.5f)) + 1),
-			(hitPos.y + 0.5f) > 0.0f ? floor(hitPos.y + 0.5f) : -(floor(abs(hitPos.y + 0.5f)) + 1),
-			(hitPos.z + 0.5f) > 0.0f ? floor(hitPos.z + 0.5f) : -(floor(abs(hitPos.z + 0.5f)) + 1)
-		};
-		m_pSelection->GetTransform()->Translate(blockPos);
-
-		// Get current block
-		Block* pBlock{ m_pWorld->GetBlockAt(static_cast<int>(blockPos.x), static_cast<int>(blockPos.y), static_cast<int>(blockPos.z)) };
-
-		if (HasChangedPosition(blockPos)) m_IsBreakingBlock = false;
-
-		if (InputManager::IsMouseButton(InputState::pressed, 2))
-		{
-			m_pWorld->PlaceBlock(XMFLOAT3{ hitPos.x, hitPos.y, hitPos.z }, blockPos, BlockType::DIRT);
-			m_IsBreakingBlock = false;
-		}
-		
-		if (m_IsBreakingBlock)
-		{
-			if (InputManager::IsMouseButton(InputState::down, 1))
+			// Destroy the block if the progress has reached 100%
+			if (m_BlockBreakProgress > pBlock->breakTime)
 			{
-				if (pBlock)
-				{
-					// Increment breaking time
-					m_BlockBreakProgress += sceneContext.pGameTime->GetElapsed();
-
-					// Destroy the block if the progress has reached 100%
-					if (m_BlockBreakProgress > pBlock->breakTime)
-					{
-						m_pWorld->DestroyBlock(blockPos);
-					}
-				}
+				m_pWorld->DestroyBlock(blockPos);
+			}
+		}
+		else
+		{
+			// Destroy the block if it has instant breaking, else reset block breaking
+			if (pBlock && pBlock->breakTime <= 0.0f)
+			{
+				m_pWorld->DestroyBlock(blockPos);
 			}
 			else
 			{
-				m_IsBreakingBlock = false;
+				// Reset breaking block progress
+				m_IsBreakingBlock = true;
+				m_BlockBreakProgress = 0.0f;
 			}
-		}
-		else if (InputManager::IsMouseButton(InputState::down, 1))
-		{
-			m_IsBreakingBlock = true;
-			m_BlockBreakProgress = 0.0f;
-		}
-
-
-		// Enable/Disable the breaking block renderer
-		m_pBreakRenderer->SetVisibility(m_IsBreakingBlock);
-
-		if (m_IsBreakingBlock && pBlock)
-		{
-			// Set the right stage to the breaking renderer
-			constexpr int nrBreakStages{ 10 };
-			m_pBreakRenderer->SetBreakingStage(std::min(static_cast<int>(m_BlockBreakProgress / pBlock->breakTime * nrBreakStages), nrBreakStages - 1));
 		}
 	}
 	else
 	{
-		m_pSelection->SetVisibility(false);
-		m_pBreakRenderer->SetVisibility(false);
+		// Disable block breaking
 		m_IsBreakingBlock = false;
+	}
+
+	// Enable/Disable the breaking block renderer
+	m_pBreakRenderer->SetVisibility(m_IsBreakingBlock);
+
+	if (m_IsBreakingBlock && pBlock)
+	{
+		// Set the right stage to the breaking renderer
+		constexpr int nrBreakStages{ 10 };
+		m_pBreakRenderer->SetBreakingStage(std::min(static_cast<int>(m_BlockBreakProgress / pBlock->breakTime * nrBreakStages), nrBreakStages - 1));
 	}
 }
 
